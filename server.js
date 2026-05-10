bf023bdded5daffcc21bf89508a176b4f3830524
Aldair Rutte <xxpiltisxx@gmail.com>
2026-05-10 17:33:52 -0500

    The rest file was uploaded2

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const env = require("dotenv").config()
 const express = require("express")
 const app = express()
 
+app.use("/", (req, res, next) => {
+    res.send("Hello World!!")
+});
+
 app
    .use(express.json())
    .use((req, res, next) => {
